3ab054c15e045970dbb0a48f7d65526fc7376628
AbhijitKr <abhijitkr600@gmail.com>
2024-02-04 16:01:57 +0530

    Adds Euclid Font Family

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import GlobalState from "./context/context.jsx";
+import "./fonts/EuclidCircularB-Regular.ttf";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
